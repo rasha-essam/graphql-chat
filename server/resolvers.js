6f26a6a1454b9683e5994cf44a463ec0cccb6003
rasha-essam <engrasha88@gmail.com>
2020-06-12 00:02:16 +0200

    Subscription Resolver with PubSub

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,4 +1,8 @@
+const { PubSub } = require('graphql-subscriptions');
 const db = require('./db');
+
+const pubSub = new PubSub(); // stands for publish - subscribe
+const MESSAGE_ADDED = 'MESSAGE_ADDED';
 
 function requireAuth(userId) {
   if (!userId) {
@@ -10,15 +14,23 @@ const Query = {
   messages: (_root, _args, { userId }) => {
     requireAuth(userId);
     return db.messages.list();
-  },
+  }
 };
 
 const Mutation = {
   addMessage: (_root, { input }, { userId }) => {
     requireAuth(userId);
     const messageId = db.messages.create({ from: userId, text: input.text });
-    return db.messages.get(messageId);
-  },
+    const message = db.messages.get(messageId);
+    pubSub.publish(MESSAGE_ADDED, { messageAdded: message });
+    return message;
+  }
 };
 
-module.exports = { Query, Mutation };
+const Subscription = {
+  messageAdded: {
+    subscribe: () => pubSub.asyncIterator(MESSAGE_ADDED)
+  }
+};
+
+module.exports = { Query, Mutation, Subscription };

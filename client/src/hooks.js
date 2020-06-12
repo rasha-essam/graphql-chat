972975e9bfd22d52a5bc303d2f7b84a79ce1b7b3
rasha-essam <engrasha88@gmail.com>
2020-06-12 17:56:51 +0200

    Local state management with Apollo Client and writing custom hooks

---
+++ client/src/hooks.js
@@ -0,0 +1,23 @@
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
+import {
+  messagesQuery,
+  addMessageMutation,
+  messageAddedSubscription
+} from './graphql/queries';
+
+export const useChatMessages = () => {
+  const { data } = useQuery(messagesQuery);
+  const messages = data ? data.messages : [];
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({ client, subscriptionData }) => {
+      client.writeData({
+        data: { messages: messages.concat(subscriptionData.data.messageAdded) }
+      });
+    }
+  });
+  const [addMessage] = useMutation(addMessageMutation);
+  return {
+    messages,
+    addMessage: (text) => addMessage({ variables: { input: { text } } })
+  };
+};
